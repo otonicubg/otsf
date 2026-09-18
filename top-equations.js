05fecb9bcce6c7419caaa39c0f3e5dd0cb31a640
otonicubg <otonicubg@gmail.com>
2026-09-18 15:18:15 -0600

    Add top equations

---
+++ top-equations.js
@@ -0,0 +1,8 @@
+const topEquations = [
+    {id: 1110, name: 'Clover pit', image: '.jpeg', path: 'Clover Pit', description: "Gambling 🤑🤑🤑"},    {id: 1114, name: 'Shellshockers', image: '.jpg', path: 'shellshock', description: "Absolute PEAK. A multiplayer game where you run around and shoot other eggs."},
+    {id: 1079, name: 'Balatro', image: '.jpeg', path: 'Balatro', description: "A hypnotically satisfying deckbuilder where you play illegal poker hands, discover game-changing jokers."},
+    {id: 1064, name: 'Buckshot Roulette', image: '.jpeg', path: 'Buckshot Roulette', description: "Play Russian roulette with a 12-gauge shotgun. Four enter. One leaves. Roll the dice with your life. Good luck!"},
+    {id: 1112, name: 'Silksong', image: '.jpg', path: 'Silksong', description: "The sequel to hollow knight :)" },
+]
+
+export default topEquations
